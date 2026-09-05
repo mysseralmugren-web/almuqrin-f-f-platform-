a785d5a6dde2fe1d3e8c93f20c72bf9663c2a03f
mysseralmugren-web <mysseralmugren@gmail.com>
2026-09-06 01:00:23 +0300

    feat(store): add realistic furniture scene details

--- src/components/storefront/atelier-scene.tsx
+++ src/components/storefront/atelier-scene.tsx
@@ -10,16 +10,20 @@ export function AtelierScene({ image }: AtelierSceneProps) {
   return (
     <div className="store-atelier" aria-hidden="true">
       <div className="store-atelier__halo" />
+      <div className="store-atelier__window-light" />
       <div className="store-atelier__floor" />
+      <div className="store-atelier__floor-lines" />
       <div className="store-atelier__cabinet">
         <div className="store-atelier__cabinet-top" />
         <div className="store-atelier__cabinet-side" />
         <div className="store-atelier__cabinet-face">
-          <div className="store-atelier__line" />
-          <div className="store-atelier__line" />
+          <div className="store-atelier__door store-atelier__door--right" />
+          <div className="store-atelier__door store-atelier__door--left" />
           <span className="store-atelier__handle store-atelier__handle--one" />
           <span className="store-atelier__handle store-atelier__handle--two" />
         </div>
+        <i className="store-atelier__leg store-atelier__leg--right" />
+        <i className="store-atelier__leg store-atelier__leg--left" />
       </div>
       <div className="store-atelier__frame">
         {image ? <img src={image} alt="" /> : <div className="store-atelier__frame-gradient" />}
